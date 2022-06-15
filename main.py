8a4b26fbd8fb4c17d943ec934b85d25ceb408475
loganflatt <loganflatt2018@gmail.com>
2022-06-15 17:12:27 -0400

    end day 2
    changed method calls and cleaned up code
    TODO: make the column names match the table trying to be edited

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 
 
 import mysql.connector
-1
 
 db = mysql.connector.connect(
     host = 'localhost',
@@ -22,6 +21,20 @@ and performs CRUD operations on a database.
 
 cursor = db.cursor()
 
+def chooseTable():
+    print('\nwhere would you like to read an entry\n')
+    choice = int(input("1 - car_table\n2 - owner_table\n3 - employees\n4 - invoice\n"))
+    if choice == 1:
+        return 'car_table'
+    elif choice == 2:
+        return 'owner_table'
+    elif choice == 3:
+        return 'employees' 
+    elif choice == 4:
+        return 'invoice'
+    else: 
+        print('Give a valid input')
+        return chooseTable()
 
 def getInputs():
     inputs = []
@@ -30,75 +43,51 @@ def getInputs():
         inputs.append(input('Chose a value to insert into column above(primary keys will not be added for car table or employees): '))
     return inputs
 
+def printTable():
+    print('\nTable:')
+    for i in cursor:
+        print(i)
 
+def delete(table):
+    key = input('Enter the primary key of the row you would like to delete: ')
+    cursor.execute(f'DELETE FROM {table} WHERE license_plate = {key}')
+    db.commit()
+    return 
 
+#create method
 def create_method():
-    #create method
-    print('\nwhere would you like to create an entry\n')
-    choice = int(input("1 - car_table\n2 - owner_table\n3 - employees\n4 - invoice\n"))
-    if choice == 1:
-        cursor.execute("DESCRIBE car_table")
-        inputs = getInputs()
-        input_tuple = (inputs[0],inputs[1],int(inputs[2]), inputs[3])
-        cursor.execute('INSERT INTO car_table (make,model,year, color) VALUES (%s, %s,%s,%s)', input_tuple)
-        db.commit()
-    if choice == 2:
-        cursor.execute("DESCRIBE owner_table")
-        inputs = getInputs()
-        input_tuple = (inputs[0],inputs[1],int(inputs[2]), int(inputs[3]))
-        cursor.execute('INSERT INTO owner_table (name, address, phone_number, plate) VALUES (%s, %s,%s,%s)', input_tuple)
-        db.commit()
-    if choice == 3:
-        cursor.execute("DESCRIBE employees")
-        inputs = getInputs()
-        input_tuple = (inputs[0],int(inputs[1]))
-        cursor.execute('INSERT INTO employees (name, salary) VALUES (%s, %s)', input_tuple)
-        db.commit()
-    if choice == 4:
-        cursor.execute("DESCRIBE invoice")
-        inputs = getInputs()
-        input_tuple = (int(inputs[0]),inputs[1],int(inputs[2]))
-        cursor.execute('INSERT INTO invoice (employee, sale_date, price) VALUES (%s, %s,%s)', input_tuple)
-        db.commit()
+    table = chooseTable()
+    cursor.execute(f"DESCRIBE {table}")
+    inputs = getInputs()
+    values = '"' + inputs[0] + '"'
+    for i in range(1,len(inputs)):
+        values += ', "' + values[i] + '"'
+    cursor.execute(f'INSERT INTO {table} (make,model,year,color) VALUES ("{values}")')
+    db.commit()
     return
 
+
+#read method
 def read_method():
-    #read method
-    print('\nwhere would you like to read an entry\n')
-    choice = int(input("1 - car_table\n2 - owner_table\n3 - employees\n4 - invoice\n"))
-    if choice == 1:
-        cursor.execute('SELECT * FROM car_table;')
-        print('Table:\n')
-        for i in cursor:
-            print(i)
-    if choice == 2:
-        cursor.execute('SELECT * FROM owner_table;')
-        print('Table:\n')
-        for i in cursor:
-            print(i)
-    if choice == 3:
-        cursor.execute('SELECT * FROM employees;')
-        print('Table:\n')
-        for i in cursor:
-            print(i)
-    if choice == 4:
-        cursor.execute('SELECT * FROM invoice;')
-        print('Table:\n')
-        for i in cursor:
-            print(i)
+    table = chooseTable()
+    cursor.execute(f'SELECT * FROM {table}')
+    printTable()
     return
 
+#update method
 def update_method():
-    #create method
-    print('\nwhere would you like to update an entry\n')
-    choice = int(input("1 - car_table\n2 - owner_table\n3 - employees\n4 - invoice\n"))
+    table = chooseTable()
     
     return
 
+#delete method
 def delete_method():
-    #create method
-    print('\nwhere would you like to delete an entry\n')
-    choice = input("1 - car_table\n2 - owner_table\n3 - employees\n4 - invoice\n")
+    table = chooseTable()
+    cursor.execute(f'SELECT * FROM {table};')
+    printTable()
+    delete('car_table')
+        
+    
     return
 
 while True:
@@ -108,6 +97,7 @@ while True:
     print('3. Update')
     print('4. Delete')
     print('5. exit')
+    
     choice = int(input("\n----Select what you would like to do to the database----\n"))
     if(choice == 1): create_method()
     elif(choice == 2): read_method()
